d1aa2b4844716c7c60705afc4cd906f3db20d011
Johnny JTH <johanohly@gmail.com>
2023-06-20 15:34:37 +0200

    Add favion and page title. Enabled PWA.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import { RenderInterceptor, SiteHeader } from "$components";
   import { setInitialClassState } from "$components/light-switch/light-switch";
+  import "../lib/utilities/string";
   import "../app.postcss";
+
+  $: title =
+    $page.url.pathname == "/"
+      ? "Aula"
+      : `${$page.url.pathname
+          .split("/")[1]
+          .replace("-", " ")
+          .toTitleCase()} - Aula`;
 </script>
 
 <svelte:head>
+  <title>{title}</title>
   <!-- This causes the new eslint-plugin-svelte: https://github.com/sveltejs/eslint-plugin-svelte/issues/492 -->
   {@html `<\u{73}cript nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
 </svelte:head>
