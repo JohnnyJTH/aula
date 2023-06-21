3db3b4f533cd0d4db763c5f7faf49a1bf80be5f6
Johnny JTH <johanohly@gmail.com>
2023-06-21 22:50:25 +0200

    Fix theme-color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,22 +14,25 @@
           .replace("-", " ")
           .toTitleCase()} - Aula`;
 
-	function scrollHeadingIntoView(): void {
-		if (!window.location.hash) return;
-		const elemTarget: HTMLElement | null = document.querySelector(window.location.hash);
-		if (elemTarget) elemTarget.scrollIntoView({ behavior: 'smooth' });
-	}
+  function scrollHeadingIntoView(): void {
+    if (!window.location.hash) return;
+    const elemTarget: HTMLElement | null = document.querySelector(
+      window.location.hash
+    );
+    if (elemTarget) elemTarget.scrollIntoView({ behavior: "smooth" });
+  }
 
-	// Lifecycle
-	afterNavigate((params: any) => {
-		// Scroll to top
-		const isNewPage: boolean = params.from && params.to && params.from.route.id !== params.to.route.id;
-		const elemPage = document.querySelector('#page');
-		if (isNewPage && elemPage !== null) {
-			elemPage.scrollTop = 0;
-		}
-		scrollHeadingIntoView();
-	});
+  // Lifecycle
+  afterNavigate((params: any) => {
+    // Scroll to top
+    const isNewPage: boolean =
+      params.from && params.to && params.from.route.id !== params.to.route.id;
+    const elemPage = document.querySelector("#page");
+    if (isNewPage && elemPage !== null) {
+      elemPage.scrollTop = 0;
+    }
+    scrollHeadingIntoView();
+  });
 </script>
 
 <svelte:head>
@@ -37,22 +40,40 @@
 
   <!-- Meta Tags -->
   <meta name="title" content={title} />
-	<meta name="description" content="Aula er en elegant og brugervenlig webside, der præsenterer data fra Lectio på en forbedret måde. " />
-	<meta name="keywords" content="aula, lectio, studieadministration, skoleskema, karakterer, fravær, beskeder, brugervenlig, æstetisk, intuitiv, pæn, overskuelig" />
-	<meta name="theme-color" content="#fff" />
-	<meta name="author" content="Johnny JTH" />
-	<!-- Open Graph - https://ogp.me/ -->
-	<meta property="og:site_name" content="Aula" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://aula.johnnyjth.com{$page.url.pathname}" />
-	<meta property="og:locale" content="da_DK" />
-	<meta property="og:title" content={title} />
-	<meta property="og:description" content="Aula er en elegant og brugervenlig webside, der præsenterer data fra Lectio på en forbedret måde. " />
-	<meta property="og:image" content="https://aula.johnnyjth.com/favicon-dark.png" />
-	<meta property="og:image:secure_url" content="https://aula.johnnyjth.com/favicon-dark.png" />
-	<meta property="og:image:type" content="image/png" />
-	<meta property="og:image:width" content="256" />
-	<meta property="og:image:height" content="256" />
+  <meta
+    name="description"
+    content="Aula er en elegant og brugervenlig webside, der præsenterer data fra Lectio på en forbedret måde. "
+  />
+  <meta
+    name="keywords"
+    content="aula, lectio, studieadministration, skoleskema, karakterer, fravær, beskeder, brugervenlig, æstetisk, intuitiv, pæn, overskuelig"
+  />
+  <meta name="theme-color" content="#ffffff" />
+  <meta name="author" content="Johnny JTH" />
+  <!-- Open Graph - https://ogp.me/ -->
+  <meta property="og:site_name" content="Aula" />
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:url"
+    content="https://aula.johnnyjth.com{$page.url.pathname}"
+  />
+  <meta property="og:locale" content="da_DK" />
+  <meta property="og:title" content={title} />
+  <meta
+    property="og:description"
+    content="Aula er en elegant og brugervenlig webside, der præsenterer data fra Lectio på en forbedret måde. "
+  />
+  <meta
+    property="og:image"
+    content="https://aula.johnnyjth.com/favicon-dark.png"
+  />
+  <meta
+    property="og:image:secure_url"
+    content="https://aula.johnnyjth.com/favicon-dark.png"
+  />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:image:width" content="256" />
+  <meta property="og:image:height" content="256" />
 
   <!-- This causes the new eslint-plugin-svelte: https://github.com/sveltejs/eslint-plugin-svelte/issues/492 -->
   {@html `<\u{73}cript nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
